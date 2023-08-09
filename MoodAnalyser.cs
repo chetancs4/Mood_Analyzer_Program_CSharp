e5fb2e06413a89c1a4d0d336cc1fc6d1d8e93642
Chetan <chetansadvelkar4@gmail.com>
2023-08-09 08:36:18 +0530

    Mood Analyser

---
+++ MoodAnalyser.cs
@@ -0,0 +1,29 @@
+﻿namespace Mood_Analyser
+{
+    public class MoodAnalyser
+    {
+        private string message;
+
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalyseMood()
+        {
+            // Basic logic to determine mood based on keywords
+            if (message.Contains("happy", StringComparison.OrdinalIgnoreCase))
+            {
+                return "Happy Mood";
+            }
+            else if (message.Contains("sad", StringComparison.OrdinalIgnoreCase))
+            {
+                return "Sad Mood";
+            }
+            else
+            {
+                return "Unable to determine mood";
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Mood_Analyser
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter a message: ");
+            string inputMessage = Console.ReadLine();
+
+            MoodAnalyser moodAnalyser = new MoodAnalyser(inputMessage);
+            string mood = moodAnalyser.AnalyseMood();
+
+            Console.WriteLine($"Mood: {mood}");
+        }
+    }
+}
