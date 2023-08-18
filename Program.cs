989f42e521a21935cd865ee749e0ff93ab74415f
Chetan <chetansadvelkar4@gmail.com>
2023-08-18 21:14:46 +0530

    Chetan: Add Eception Handler.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Mood Analyzer Program");
+            Console.WriteLine("Welcome to Mood Analyzer Program.");
             MoodAnalyzer mood = new MoodAnalyzer("I am Happy");
             Console.WriteLine("Mood is " + mood.AnalyseMood());
         }
